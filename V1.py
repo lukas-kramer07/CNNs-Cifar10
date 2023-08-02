fcb4fec2ea32a4ea6922ef171c313742c74fabfa
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-08-02 21:32:05 +0200

    compilation and fitting

--- V1.py
+++ V1.py
@@ -18,3 +18,10 @@ model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10))
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=10, 
+                    validation_data=(test_images, test_labels))
