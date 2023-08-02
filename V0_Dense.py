1442c5d308c7a04beacad8a0fb55d677f61b696b
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-08-02 18:01:56 +0200

    Dense test added

---
+++ V0_Dense.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+# Convert class labels to one-hot encoded vectors
+num_classes = 10
+train_labels = tf.keras.utils.to_categorical(train_labels, num_classes)
+test_labels = tf.keras.utils.to_categorical(test_labels, num_classes)
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i])
+    # The CIFAR labels happen to be arrays, 
+    # which is why you need the extra index
+    plt.xlabel(class_names[train_labels[i].argmax()])
+plt.show()
+
+model = tf.keras.Sequential([
+    layers.Flatten(input_shape=(32,32,3)),
+    layers.Dense(128, activation="relu"),
+    layers.Dense(200, activation="relu"),
+    layers.Dense(80, activation="relu"),
+    layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(train_images, train_labels, epochs=60, validation_data=(test_images, test_labels))
+
+# Evaluate the model on the test data
+loss, accuracy = model.evaluate(test_images, test_labels)
+print(f'Test loss: {loss:.4f}, Test accuracy: {accuracy:.4f}')
