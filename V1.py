d9234780140c6d402ae7c0634dfd56ebe1f0e6db
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-08-03 15:24:02 +0200

    bigger model V1

--- V1.py
+++ V1.py
@@ -13,17 +13,17 @@ model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
 model.add(layers.Dense(10))
 
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-history = model.fit(train_images, train_labels, epochs=10, 
+history = model.fit(train_images, train_labels, epochs=30, 
                     validation_data=(test_images, test_labels))
 
 plt.plot(history.history['accuracy'], label='accuracy')
