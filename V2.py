f577f0cee8f4cb0e7c6e8e1d564d29eb9943dee7
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-08-03 21:38:06 +0200

    V2 trained

--- V2.py
+++ V2.py
@@ -1,8 +1,58 @@
 
 import tensorflow as tf
-
 from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+class_names = ["T-shirt/top","Trousers", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle Boot"]
+# Create a confusion matrix
+# Note: Adapted from scikit-learn's plot_confusion_matrix()
+
+import itertools
+from sklearn.metrics import confusion_matrix
+
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10,10,), text_size=15):
+  #Create the confusion matrix
+  cm = confusion_matrix(y_true, tf.round(y_preds))
+  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
+  n_classes= cm.shape[0]
+
+  # Let's prettify it
+  fig, ax = plt.subplots(figsize=figsize)
+  #Create a matrix plot
+  cax = ax.matshow(cm, cmap=plt.cm.Purples)
+  fig.colorbar(cax)
+
+#set labels to be classes
+  labels = classes if classes else np.arange(cm.shape[0])
+  # Label the axes
+  ax.set(title="Confusion matrix - Fashion_modelV1",
+        xlabel="Predicted label",
+        ylabel="True label",
+        xticks=np.arange(n_classes),
+        yticks=np.arange(n_classes),
+        xticklabels=labels,
+        yticklabels=labels)
+
+  # Set x-labels to the bottom
+  ax.xaxis.set_label_position("bottom")
+  ax.xaxis.tick_bottom()
+
+  #Adjust label size
+  ax.xaxis.label.set_size(text_size)
+  ax.yaxis.label.set_size(text_size)
+  ax.title.set_size(text_size)
+
+  # Set coluor threshhold
+  threshold = (cm.max() + cm.min())/2.
+
+  #Plot the text on each cell
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(j, i, f"{cm[i, j]} ({cm_norm[i,j]*100:.1f}%)",
+            horizontalalignment="center",
+            color="white" if cm[i, j] > threshold else "black",
+            size=text_size)
 
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
@@ -35,7 +85,18 @@ plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.ylim([0.5, 1])
 plt.legend(loc='lower right')
-plt.show()
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 print(test_acc)
 model.summary()
+model.save("V2")
+
+y_probs = model.predict(test_images)
+y_preds = tf.argmax(y_probs, axis=1)
+
+
+make_confusion_matrix(y_true=test_labels,
+                      y_pred=y_preds,
+                      classes=class_names,
+                      figsize=(13,13),
+                      text_size=8)
+plt.show()
