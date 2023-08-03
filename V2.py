6443eb0ea9bf3fbd255a0d81a19c56cc37eb3cb3
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-08-03 22:28:48 +0200

    class names

--- V2.py
+++ V2.py
@@ -53,8 +53,7 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10,10,), text_s
             size=text_size)
 
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck']
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer','dog', 'frog', 'horse', 'ship', 'truck']
 # Normalize pixel values to be between 0 and 1
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
