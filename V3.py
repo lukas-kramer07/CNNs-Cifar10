fb4d9ea8e0ffae075d8da434dd882ab64b227cbd
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-08-05 10:09:42 +0200

    V3 results

--- V3.py
+++ V3.py
@@ -74,7 +74,6 @@ def main():
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(128, (3, 3), activation='relu'))
     model.add(layers.Flatten())
     model.add(layers.Dense(64, activation='relu'))
