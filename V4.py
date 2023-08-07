6e89cad03f69bf1b26eaebce13daff7a20ead6e3
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-08-07 15:08:43 +0200

    feat: add utils.make_confusion_matrix (V4)

--- V4.py
+++ V4.py
@@ -12,48 +12,6 @@ import utils
 model_name = 'V4'
 # Create a confusion matrix
 # Note: Adapted from scikit-learn's plot_confusion_matrix()
-def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10,10,), text_size=15):
-  #Create the confusion matrix
-  cm = confusion_matrix(y_true, tf.round(y_pred))
-  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
-  n_classes= cm.shape[0]
-
-  # Let's prettify it
-  fig, ax = plt.subplots(figsize=figsize)
-  #Create a matrix plot
-  cax = ax.matshow(cm, cmap=plt.cm.Purples)
-  fig.colorbar(cax)
-
-#set labels to be classes
-  labels = classes if classes else np.arange(cm.shape[0])
-  # Label the axes
-  ax.set(title="Confusion matrix - Fashion_modelV1",
-        xlabel="Predicted label",
-        ylabel="True label",
-        xticks=np.arange(n_classes),
-        yticks=np.arange(n_classes),
-        xticklabels=labels,
-        yticklabels=labels)
-
-  # Set x-labels to the bottom
-  ax.xaxis.set_label_position("bottom")
-  ax.xaxis.tick_bottom()
-
-  #Adjust label size
-  ax.xaxis.label.set_size(text_size)
-  ax.yaxis.label.set_size(text_size)
-  ax.title.set_size(text_size)
-
-  # Set coluor threshhold
-  threshold = (cm.max() + cm.min())/2.
-
-  #Plot the text on each cell
-  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-    plt.text(j, i, f"{cm[i, j]} ({cm_norm[i,j]*100:.1f}%)",
-            horizontalalignment="center",
-            color="white" if cm[i, j] > threshold else "black",
-            size=text_size)
-
 def main():
   (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
   class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer','dog', 'frog', 'horse', 'ship', 'truck']
@@ -98,7 +56,7 @@ def main():
   train_images, test_images = train_images / 255.0, test_images / 255.0
   y_probs = model.predict(test_images)
   y_preds = tf.argmax(y_probs, axis=1)
-  make_confusion_matrix(y_true=test_labels,
+  utils.make_confusion_matrix(y_true=test_labels,
                         y_pred=y_preds,
                         classes=class_names,
                         figsize=(13,13),
