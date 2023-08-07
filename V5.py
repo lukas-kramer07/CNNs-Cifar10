888f238f385fddd162564a1747a061efa18b4bc9
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-08-07 22:36:22 +0200

    feat: add in turn Data Augmenter (V5)

--- V5.py
+++ V5.py
@@ -4,6 +4,8 @@
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.utils import to_categorical
 import os
 import utils
 model_name = 'V5'
@@ -25,43 +27,53 @@ def create_model():
   return model
 
 def main():
-  (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
-  class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer','dog', 'frog', 'horse', 'ship', 'truck']
-  # Normalize pixel values to be between 0 and 1
-  train_images, test_images = train_images / 255.0, test_images / 255.0
-
-  model = create_model()
-  model.compile(optimizer='adam',
-                loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+    (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+    class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer','dog', 'frog', 'horse', 'ship', 'truck']
+    # Normalize pixel values to be between 0 and 1
+    train_images, test_images = train_images / 255.0, test_images / 255.0
+    # Create a data generator with data augmentation
+    train_labels_hot = to_categorical(train_labels, 10)
+    test_labels_hot = to_categorical(test_labels, 10)
+    train_datagen = ImageDataGenerator(
+        #preprocessing_function=utils.augment,
+        rotation_range=5,
+        #zoom_range=[0.8, 1.2],
+        #shear_range=0.2,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+    )
+    train_datagen.fit(train_images)
+    model = create_model()
+    model.compile(optimizer='adam',
+                loss=tf.keras.losses.CategoricalCrossentropy(),
                 metrics=['accuracy'])
+    history = model.fit(train_datagen.flow(train_images, train_labels_hot, batch_size=32), epochs=20,
+                        validation_data=(test_images, test_labels_hot))
 
-  history = model.fit(train_images, train_labels, epochs=20, 
-                      validation_data=(test_images, test_labels))
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+    os.makedirs(f"plots/{model_name}", exist_ok=True)  # Create the "models" folder if it doesn't exist
+    plt.savefig(f"plots/{model_name}/history")
 
-  plt.plot(history.history['accuracy'], label='accuracy')
-  plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-  plt.xlabel('Epoch')
-  plt.ylabel('Accuracy')
-  plt.ylim([0.5, 1])
-  plt.legend(loc='lower right')
-  os.makedirs(f"plots/{model_name}", exist_ok=True)  # Create the "models" folder if it doesn't exist
-  plt.savefig(f"plots/{model_name}/history")
-
-  y_probs = model.predict(test_images)
-  y_preds = tf.argmax(y_probs, axis=1)
-  utils.make_confusion_matrix(y_true=test_labels,
+    y_probs = model.predict(test_images)
+    y_preds = tf.argmax(y_probs, axis=1)
+    utils.make_confusion_matrix(y_true=test_labels,
                         y_pred=y_preds,
                         classes=class_names,
                         figsize=(13,13),
                         text_size=8)
-  os.makedirs(f"plots/{model_name}", exist_ok=True)  # Create the "models" folder if it doesn't exist
-  plt.savefig(f"plots/{model_name}/confusion_matrix")
-  
-  test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
-  print(f"test_acc: {test_acc}; test_loss: {test_loss}")
-  model.summary()
-  os.makedirs(f"models", exist_ok=True)  # Create the "models" folder if it doesn't exist
-  model.save(f"models/{model_name}")
+    os.makedirs(f"plots/{model_name}", exist_ok=True)  # Create the "models" folder if it doesn't exist
+    plt.savefig(f"plots/{model_name}/confusion_matrix")
+
+    test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+    print(f"test_acc: {test_acc}; test_loss: {test_loss}")
+    model.summary()
+    os.makedirs(f"models", exist_ok=True)  # Create the "models" folder if it doesn't exist
+    model.save(f"models/{model_name}")
 
 if __name__ == "__main__":
   main()
