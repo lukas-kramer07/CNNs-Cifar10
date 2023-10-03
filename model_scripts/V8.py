bfaf0291a76feb9b2071161f9546b7d7450bdad2
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-10-03 11:07:59 +0200

    feat: adapt cm for tfds usage

--- model_scripts/V8.py
+++ model_scripts/V8.py
@@ -7,6 +7,7 @@ from V2 import create_model
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import tensorflow_datasets as tfds
+import numpy as np
 model_name = 'V8'
 IM_SIZE = 32
 BATCH_SIZE = 32
@@ -25,7 +26,7 @@ def main():
                 loss=tf.keras.losses.CategoricalCrossentropy(),
                 metrics=['accuracy'])
     #train for 20 epochs
-    history = model.fit(train_ds, epochs=3, validation_data=test_ds)
+    history = model.fit(train_ds, epochs=10, validation_data=test_ds)
 
     #model_evaluation
     model_eval(history=history, model=model,model_name=model_name, test_ds=test_ds)
@@ -55,24 +56,26 @@ def resize_rescale(Image, Label):
 
 ## Evaluation
 def model_eval(model, model_name, history, test_ds):
-    utils.change_plot(history)
+    '''utils.change_plot(history)
     # Create the "plots" folder if it doesn't exist
     os.makedirs(f"plots/{model_name}", exist_ok=True)
-    plt.savefig(f"plots/{model_name}/history_with_lr_and_change.png")
+    plt.savefig(f"plots/{model_name}/history_with_lr_and_change.png")'''
 
-
-
-
-    '''y_probs = model.predict(test_images)
+    y_probs = model.predict(test_ds)
     y_preds = tf.argmax(y_probs, axis=1)
-    utils.make_confusion_matrix(y_true=test_labels,
+    print(type(y_preds))
+    print(y_preds)
+    print(test_ds)
+    y_true = np.concatenate([y for x, y in test_ds], axis=0) # extract labels from test_ds
+    y_true = tf.argmax(y_true, axis=1) # revert from one_hot
+    utils.make_confusion_matrix(y_true=y_true,
                         y_pred=y_preds,
                         classes=class_names,
                         figsize=(13,13),
                         text_size=8,
                         model_name=model_name)
     os.makedirs(f"plots/{model_name}", exist_ok=True)  # Create the "models" folder if it doesn't exist
-    plt.savefig(f"plots/{model_name}/confusion_matrix")'''
+    plt.savefig(f"plots/{model_name}/confusion_matrix")
 
     test_loss, test_acc = model.evaluate(test_ds, verbose=1)
     print(f"test_acc: {test_acc}; test_loss: {test_loss}")
