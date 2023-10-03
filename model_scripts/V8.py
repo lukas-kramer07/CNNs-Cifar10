397e91a06bae6485811a7207253235a9e5ac9e69
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-10-03 10:05:12 +0200

    feat: implement utils.change_plot in V8

--- model_scripts/V8.py
+++ model_scripts/V8.py
@@ -18,12 +18,16 @@ def main():
     (train_ds, test_ds), ds_info= tfds.load('cifar10', split=['train','test'], as_supervised=True, with_info=True)
     #preprocess
     train_ds, test_ds = preprocess_data(train_ds, test_ds)
+
+
     model = create_model()
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
                 loss=tf.keras.losses.CategoricalCrossentropy(),
                 metrics=['accuracy'])
     #train for 20 epochs
-    history = model.fit(train_ds, epochs=20, validation_data=test_ds)
+    history = model.fit(train_ds, epochs=3, validation_data=test_ds)
+
+    #model_evaluation
     model_eval(history=history, model=model,model_name=model_name, test_ds=test_ds)
 
 ## Preprocessing the dataset
@@ -51,41 +55,7 @@ def resize_rescale(Image, Label):
 
 ## Evaluation
 def model_eval(model, model_name, history, test_ds):
-    # Calculate the change in accuracy from the previous epoch
-    accuracy_changes = [0] + [history.history['accuracy'][i] - history.history['accuracy'][i-1] for i in range(1, len(history.history['accuracy']))]
-
-    plt.figure(figsize=(10, 6))
-
-    # Plot accuracy and validation accuracy
-    accuracy_line, = plt.plot(history.history['accuracy'], label='accuracy', color='b')
-    val_accuracy_line, = plt.plot(history.history['val_accuracy'], label='val_accuracy', color='g')
-
-    # Plot change in accuracy
-    accuracy_change_line, = plt.plot(accuracy_changes, label='Accuracy Change', color='r', linestyle='dashed')
-
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.ylim([0, 1.1])
-
-    if 'lr' in history.history:
-      # Create a twin axis for the learning rate
-      ax2 = plt.gca().twinx()
-      lr_line, = ax2.plot(history.history['lr'], label='Learning Rate', color='m', linestyle='dotted')
-      ax2.set_ylabel('Learning Rate')
-
-      # Combine the legend entries from both axes
-      lines = [accuracy_line, val_accuracy_line, accuracy_change_line, lr_line]
-      labels = [line.get_label() for line in lines]
-      plt.legend(lines, labels, loc='upper left')
-      plt.title('Accuracy, Validation Accuracy, Accuracy Change, and Learning Rate')
-      plt.tight_layout()
-    else:
-      lines = [accuracy_line, val_accuracy_line, accuracy_change_line]
-      labels = [line.get_label() for line in lines]
-      plt.legend(lines, labels, loc='upper left')
-      plt.title('Accuracy, Validation Accuracy, Accuracy Change')
-      plt.tight_layout()
-
+    utils.change_plot(history)
     # Create the "plots" folder if it doesn't exist
     os.makedirs(f"plots/{model_name}", exist_ok=True)
     plt.savefig(f"plots/{model_name}/history_with_lr_and_change.png")
