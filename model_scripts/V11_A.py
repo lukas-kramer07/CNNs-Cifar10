7be292819a991ef14a4996ae18239dbb0fca7eff
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-10-07 08:46:58 +0200

    style: format document

--- model_scripts/V11_A.py
+++ model_scripts/V11_A.py
@@ -1,32 +1,54 @@
-'''
+"""
   uses V2 model and V10 callbacks as well as augmentation 
-'''
-import utils
-from V2 import create_model
-import tensorflow as tf
-from keras import backend as K
-import matplotlib.pyplot as plt
-import tensorflow_datasets as tfds
-import numpy as np
-from tensorflow.keras.callbacks import EarlyStopping, LearningRateScheduler, ModelCheckpoint, ReduceLROnPlateau, TensorBoard
+"""
 import datetime
-model_name = 'V8'
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import utils
+from keras import backend as K
+from tensorflow.keras.callbacks import (
+    EarlyStopping,
+    LearningRateScheduler,
+    ModelCheckpoint,
+    ReduceLROnPlateau,
+    TensorBoard,
+)
+from V2 import create_model
+
+model_name = "V8"
 IM_SIZE = 32
 BATCH_SIZE = 32
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer','dog', 'frog', 'horse', 'ship', 'truck']
+class_names = [
+    "airplane",
+    "automobile",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+]
 
 
 def main():
-    #load dataset
-    (train_ds, test_ds), ds_info= tfds.load('cifar10', split=['train','test'], as_supervised=True, with_info=True)
-    #preprocess
+    # load dataset
+    (train_ds, test_ds), ds_info = tfds.load(
+        "cifar10", split=["train", "test"], as_supervised=True, with_info=True
+    )
+    # preprocess
     train_ds, test_ds = preprocess_data(train_ds, test_ds)
-    #visualize new data
+    # visualize new data
     visualize_data(train_ds=train_ds, test_ds=test_ds, ds_info=ds_info)
-    #define callbacks
-    #custom TensorBoard callback
+    # define callbacks
+    # custom TensorBoard callback
     Current_Time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    LOG_DIR = './logs/fit/'+ Current_Time
+    LOG_DIR = "./logs/fit/" + Current_Time
+
     class LRTensorBoard(TensorBoard):
         # add other arguments to __init__ if you need
         def __init__(self, log_dir, **kwargs):
@@ -34,29 +56,40 @@ def main():
 
         def on_epoch_end(self, epoch, logs=None):
             logs = logs or {}
-            logs.update({'lr': K.eval(self.model.optimizer.lr)})
+            logs.update({"lr": K.eval(self.model.optimizer.lr)})
             super().on_epoch_end(epoch, logs)
 
-    
     es_callback = EarlyStopping(
-        monitor = 'val_accuracy', min_delta=0, patience=5, verbose=1, mode='auto', baseline=None, restore_best_weights = True 
+        monitor="val_accuracy",
+        min_delta=0,
+        patience=5,
+        verbose=1,
+        mode="auto",
+        baseline=None,
+        restore_best_weights=True,
     )
+
     def scheduler(epoch, lr):
         if epoch <= 3:
             lr = lr
         elif epoch % 3 == 0:
             lr = (lr * tf.math.exp(-0.3)).numpy()
         return lr
-    scheduler_callback = LearningRateScheduler(scheduler, verbose=1) 
+
+    scheduler_callback = LearningRateScheduler(scheduler, verbose=1)
     checkpoint_callback = ModelCheckpoint(
-        f'model_checkpoints/training_checkpoints/{model_name}', monitor = 'val_loss', verbose=0, save_best_only=True, mode='auto'
-    ) 
+        f"model_checkpoints/training_checkpoints/{model_name}",
+        monitor="val_loss",
+        verbose=0,
+        save_best_only=True,
+        mode="auto",
+    )
     plateau_callback = ReduceLROnPlateau(
-        monitor='val_accuracy',
+        monitor="val_accuracy",
         factor=0.1,
         patience=3,
         verbose=1,
-        mode='auto',
+        mode="auto",
         min_delta=0.0001,
         cooldown=0,
         min_lr=0,
@@ -64,38 +97,59 @@ def main():
 
     # Create and compile model
     model = create_model()
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-                loss=tf.keras.losses.CategoricalCrossentropy(),
-                metrics=['accuracy'])
-    #train for 20 epochs
-    history = model.fit(train_ds, epochs=20, validation_data=test_ds, callbacks=[es_callback, scheduler_callback, checkpoint_callback, plateau_callback,  LRTensorBoard(log_dir=LOG_DIR)])
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+        loss=tf.keras.losses.CategoricalCrossentropy(),
+        metrics=["accuracy"],
+    )
+    # train for 20 epochs
+    history = model.fit(
+        train_ds,
+        epochs=20,
+        validation_data=test_ds,
+        callbacks=[
+            es_callback,
+            scheduler_callback,
+            checkpoint_callback,
+            plateau_callback,
+            LRTensorBoard(log_dir=LOG_DIR),
+        ],
+    )
 
-    #model_evaluation
-    utils.model_eval(history=history, model=model,model_name=model_name, test_ds=test_ds, class_names=class_names)
+    # model_evaluation
+    utils.model_eval(
+        history=history,
+        model=model,
+        model_name=model_name,
+        test_ds=test_ds,
+        class_names=class_names,
+    )
+
 
 ## Preprocessing the dataset
 def preprocess_data(train_ds, test_ds):
-        AUTOTUNE = tf.data.experimental.AUTOTUNE
-        train_ds = (
-             train_ds
-             .map(resize_rescale, num_parallel_calls=AUTOTUNE)
-             .cache()
-             .shuffle(8, reshuffle_each_iteration=True)
-             .map(augment, num_parallel_calls=AUTOTUNE)
-             .batch(BATCH_SIZE)
-             .prefetch(AUTOTUNE)  
-            )
-        test_ds = (
-             test_ds
-             .map(resize_rescale, num_parallel_calls=AUTOTUNE)
-             .batch(BATCH_SIZE)
-             .prefetch(AUTOTUNE) 
-            )
-        return train_ds, test_ds
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
+    train_ds = (
+        train_ds.map(resize_rescale, num_parallel_calls=AUTOTUNE)
+        .cache()
+        .shuffle(8, reshuffle_each_iteration=True)
+        .map(augment, num_parallel_calls=AUTOTUNE)
+        .batch(BATCH_SIZE)
+        .prefetch(AUTOTUNE)
+    )
+    test_ds = (
+        test_ds.map(resize_rescale, num_parallel_calls=AUTOTUNE)
+        .batch(BATCH_SIZE)
+        .prefetch(AUTOTUNE)
+    )
+    return train_ds, test_ds
+
+
 def resize_rescale(Image, Label):
-    Image = tf.image.resize(Image,(IM_SIZE,IM_SIZE))
-    Label = tf.one_hot(Label, len(class_names)) #one_hot_encode
-    return Image/255.0, Label
+    Image = tf.image.resize(Image, (IM_SIZE, IM_SIZE))
+    Label = tf.one_hot(Label, len(class_names))  # one_hot_encode
+    return Image / 255.0, Label
+
 
 @tf.function
 def augment(Image, Label):
@@ -106,8 +160,9 @@ def augment(Image, Label):
         Image = tf.image.grayscale_to_rgb(Image)
     Image = tf.image.random_brightness(Image, 0.2)
     Image = tf.image.random_contrast(Image, 0.7, 1.4)
-    #Image = tf.image.random_saturation(Image, 0,0.5)
+    # Image = tf.image.random_saturation(Image, 0,0.5)
     return Image, Label
+
 
 def visualize_data(train_ds, test_ds, ds_info):
     num_images_to_display = 15
@@ -117,9 +172,16 @@ def visualize_data(train_ds, test_ds, ds_info):
     for i in range(int(np.ceil(num_images_to_display / BATCH_SIZE))):
         image, label = next(iter(test_ds))
         for n in range(min(BATCH_SIZE, num_images_to_display - i * BATCH_SIZE)):
-            ax = plt.subplot(2*int(tf.sqrt(float(num_images_to_display))) + 1, 2*int(tf.sqrt(float(num_images_to_display))) + 1, n + i + 1)
-            plt.imshow(image[n]) 
-            plt.title(f"Test - {ds_info.features['label'].int2str(int(tf.argmax(label[n])))}", fontsize=10)
+            plt.subplot(
+                2 * int(tf.sqrt(float(num_images_to_display))) + 1,
+                2 * int(tf.sqrt(float(num_images_to_display))) + 1,
+                n + i + 1,
+            )
+            plt.imshow(image[n])
+            plt.title(
+                f"Test - {ds_info.features['label'].int2str(int(tf.argmax(label[n])))}",
+                fontsize=10,
+            )
             plt.axis("off")
             count += 1
 
@@ -127,13 +189,21 @@ def visualize_data(train_ds, test_ds, ds_info):
     for k in range(int(np.ceil(num_images_to_display / BATCH_SIZE))):
         image, label = next(iter(train_ds))
         for n in range(min(BATCH_SIZE, num_images_to_display - k * BATCH_SIZE)):
-            ax = plt.subplot(2*int(tf.sqrt(float(num_images_to_display))) + 1, 2*int(tf.sqrt(float(num_images_to_display))) + 1, n + i + count + 1) 
-            plt.imshow(image[n]) 
-            plt.title(f"Train - {ds_info.features['label'].int2str(int(tf.argmax(label[n])))}", fontsize=10)
+            plt.subplot(
+                2 * int(tf.sqrt(float(num_images_to_display))) + 1,
+                2 * int(tf.sqrt(float(num_images_to_display))) + 1,
+                n + i + count + 1,
+            )
+            plt.imshow(image[n])
+            plt.title(
+                f"Train - {ds_info.features['label'].int2str(int(tf.argmax(label[n])))}",
+                fontsize=10,
+            )
             plt.axis("off")
     plt.suptitle("Train and Test Samples", fontsize=14)
     plt.show()
 
-if __name__ == "__main__": 
-  main()
-  plt.show()  
+
+if __name__ == "__main__":
+    main()
+    plt.show()
