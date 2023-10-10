e1e8d297dc3dae1c8d13eca58903976030441cc6
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-10-10 20:38:06 +0200

    test: add result of RandomFlip and RandomTranslation and RandomZoom

--- model_scripts/V11_B.py
+++ model_scripts/V11_B.py
@@ -76,7 +76,7 @@ def create_augment_layers():
             #RandomRGB(prob=0.1),
             #RandomHue(factor=0.05),
             RandomTranslation(height_factor=0.1, width_factor=0.1),
-            #RandomZoom(height_factor=(-0.2, 0.1), width_factor=(-0.2, 0.1), fill_mode="reflect"),
+            RandomZoom(height_factor=(-0.1, 0.1), width_factor=(-0.1, 0.1), fill_mode="reflect"),
             #RandomBrightness(factor=0.2, value_range=(0, 1)),
         ],
         name="augment_layers",
