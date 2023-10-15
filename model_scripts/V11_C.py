54643eada27450ce4549320e2c6c855d32fff243
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-10-15 17:18:28 +0200

    test: add results for mixup+regular ds

--- model_scripts/V11_C.py
+++ model_scripts/V11_C.py
@@ -153,8 +153,10 @@ def preprocess_data(train_ds, test_ds):
     train_ds1 = train_ds.shuffle(buffer_size=32, reshuffle_each_iteration=True).map(resize_rescale)
     train_ds2 = train_ds.shuffle(buffer_size=32, reshuffle_each_iteration=True).map(resize_rescale)
     mixed_ds = tf.data.Dataset.zip((train_ds1, train_ds2)).cache()
-
-    train_ds = mixed_ds.map(mixup, num_parallel_calls=AUTOTUNE).batch(BATCH_SIZE).prefetch(AUTOTUNE)
+    train_ds = train_ds.shuffle(buffer_size=32, reshuffle_each_iteration=True).map(resize_rescale)
+    train_ds = (
+        mixed_ds.map(mixup, num_parallel_calls=AUTOTUNE).concatenate(train_ds).batch(BATCH_SIZE).prefetch(AUTOTUNE)
+    )
 
     test_ds = test_ds.map(resize_rescale, num_parallel_calls=AUTOTUNE).batch(BATCH_SIZE).prefetch(AUTOTUNE)
     return train_ds, test_ds
