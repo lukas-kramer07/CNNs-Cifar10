e10c2a38e96aaf6b31551b6260c9b605577b7b9a
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-11-05 10:54:53 +0100

    docs: add documentation

---
+++ model_scripts/V12_A.py
@@ -0,0 +1,6 @@
+'''
+This is the first of three scripts to improve the model architecture using HP-search. This script will use the grid search method
+'''
+
+import tensorflow as tf
+from tensorboard.plugins.hparams import api as hp
