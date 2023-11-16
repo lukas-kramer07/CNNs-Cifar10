918ab3db7266ffb18e98005500a8a1c00c56d638
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-11-16 11:06:02 +0100

    feat: add test_model_function

--- model_scripts/V12_D.py
+++ model_scripts/V12_D.py
@@ -1,3 +1,125 @@
-'''
+"""
 This script is a comparison of the hp models developed in V12 A to C
-'''
+"""
+"""
+  uses V2 model and includes the tensorboard_callback
+"""
+import datetime
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import utils
+from V11_E import preprocess_data
+from V11_E import visualize_data
+from keras import backend as K
+from keras.callbacks import (
+    EarlyStopping,
+    LearningRateScheduler,
+    ModelCheckpoint,
+    ReduceLROnPlateau,
+    TensorBoard,
+)
+IM_SIZE = 32
+BATCH_SIZE = 32
+class_names = [
+    "airplane",
+    "automobile",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+]
+
+
+def main():
+    # load dataset
+    (train_ds, test_ds), ds_info = tfds.load(
+        "cifar10", split=["train", "test"], as_supervised=True, with_info=True
+    )
+    # preprocess
+    train_ds, test_ds = preprocess_data(train_ds, test_ds)
+
+    
+def test_model(model, model_name, train_ds, test_ds):    
+    # define callbacks
+
+    # custom TensorBoard callback
+    Current_Time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    LOG_DIR = "./logs/fit/" + Current_Time
+
+    class LRTensorBoard(TensorBoard):
+        # add other arguments to __init__ if you need
+        def __init__(self, log_dir, **kwargs):
+            super().__init__(log_dir=log_dir, histogram_freq=3, **kwargs)
+
+        def on_epoch_end(self, epoch, logs=None):
+            logs = logs or {}
+            logs.update({"lr": K.eval(self.model.optimizer.lr)})
+            super().on_epoch_end(epoch, logs)
+
+    es_callback = EarlyStopping(
+        monitor="val_accuracy",
+        min_delta=0,
+        patience=5,
+        verbose=1,
+        mode="auto",
+        baseline=None,
+        restore_best_weights=True,
+    )
+
+    def scheduler(epoch, lr):
+        if epoch < 3:
+            lr = lr
+        else:
+            lr = (lr * tf.math.exp(-0.1)).numpy()
+        return lr
+
+    scheduler_callback = LearningRateScheduler(scheduler, verbose=1)
+    checkpoint_callback = ModelCheckpoint(
+        f"model_checkpoints/training_checkpoints/{model_name}",
+        monitor="val_loss",
+        verbose=0,
+        save_best_only=True,
+        mode="auto",
+    )
+    plateau_callback = ReduceLROnPlateau(
+        monitor="val_accuracy",
+        factor=0.1,
+        patience=3,
+        verbose=1,
+        mode="auto",
+        min_delta=0.0001,
+        cooldown=0,
+        min_lr=0,
+    )
+    # train for 20 epochs
+    history = model.fit(
+        train_ds,
+        epochs=30,
+        validation_data=test_ds,
+        callbacks=[
+            es_callback,
+            scheduler_callback,
+            checkpoint_callback,
+            plateau_callback,
+            LRTensorBoard(log_dir=LOG_DIR),
+        ],
+    )
+    # model_evaluation
+    utils.model_eval(
+        history=history,
+        model=model,
+        model_name=model_name,
+        test_ds=test_ds,
+        class_names=class_names,
+    )
+
+
+if __name__ == "__main__":
+    main()
+    # plt.show()
