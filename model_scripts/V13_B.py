edb2d6c9f1d2d353157d0a007d66be091054b486
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-11-21 20:37:01 +0100

    test: add results for Layer-subclass with singel batch norm

--- model_scripts/V13_B.py
+++ model_scripts/V13_B.py
@@ -76,7 +76,32 @@ class_names = [
         x1 = Layers.add([x, x1])
         x1 = self.relu(x1)
         return x1'''
+class ResBlock(Layer):
+        def __init__(self, channels, stride=1, name="res_block"):
+            super(ResBlock, self).__init__(name=name)
 
+            self.res_conv = stride != 1
+            self.conv1 = Conv2D(
+                filters=channels, kernel_size=3, strides=stride, padding="same"
+            )
+            self.conv2 = Conv2D(filters=channels, kernel_size=3, padding="same")
+            self.norm = BatchNormalization()
+            self.relu = ReLU()
+            if self.res_conv:
+                self.conv3 = Conv2D(filters=channels, kernel_size=1, strides=stride)
+
+        def call(self, input, training):
+            x = self.conv1(input)
+            x = self.norm(x, training)
+            x= self.relu(x)
+            x = self.conv2(x)
+            x = self.norm(x, training)
+
+            if self.res_conv:
+                input = self.conv3(input)
+                input = self.norm(input, training)
+            result = Add()([x, input])
+            return self.relu(result)
 
 def main():
     """main function that uses preprocess_data and visualize_data from V11_E to prepare the dataset. It then tests all V12 models."""
